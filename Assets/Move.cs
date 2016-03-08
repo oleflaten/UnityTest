aac71ff32b0b0fcba5343f3b08308aac3df3ae8a
Ole Flaten <ole.flaten@hihm.no>
2016-03-08 02:35:58 +0100

    Initial commit
    First commit

---
+++ Assets/Move.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	private int delay = 0;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		Debug.Log ("Hei BitBucket!");
+		delay++;
+
+		if (delay == 600)
+			SceneManager.LoadScene(1);
+
+		print ("delay " + delay);
+	
+	}
+}
